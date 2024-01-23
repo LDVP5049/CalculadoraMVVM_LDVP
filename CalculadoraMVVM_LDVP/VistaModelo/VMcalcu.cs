a803b719f490cc24ee9298a99a826bf16cf7dfa1
LDVP5049 <luismaster752@gmail.com>
2024-01-22 19:05:38 -0700

    Agregar archivos de proyecto.

---
+++ CalculadoraMVVM_LDVP/VistaModelo/VMcalcu.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace CalculadoraMVVM_LDVP.VistaModelo
+{
+    public class VMcalcu : BaseViewModel
+    {
+        #region VARIABLES
+        string _Texto;
+        #endregion
+        #region CONSTRUCTOR
+        public VMcalcu(INavigation navigation)
+        {
+            Navigation = navigation;
+        }
+        #endregion
+        #region OBJETOS
+        public string Texto
+        {
+            get { return _Texto; }
+            set { SetValue(ref _Texto, value); }
+        }
+        #endregion
+        #region PROCESOS
+        public async Task ProcesoAsyncrono()
+        {
+
+        }
+        public void ProcesoSimple()
+        {
+
+        }
+        #endregion
+        #region COMANDOS
+        public ICommand ProcesoAsyncommand => new Command(async () => await ProcesoAsyncrono());
+        public ICommand ProcesoSimpCommand => new Command(ProcesoSimple);
+        #endregion
+    }
+}
---
+++ CalculadoraMVVM_LDVP/Vistas/Vcalcu.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculadoraMVVM_LDVP.VistaModelo;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CalculadoraMVVM_LDVP.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Vcalcu : ContentPage
+    {
+        public Vcalcu()
+        {
+            InitializeComponent();
+            BindingContext = new VMcalcu(Navigation);
+        }
+    }
+}
